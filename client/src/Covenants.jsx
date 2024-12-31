525f4140ce1d2f71f9123b8316b5282f6c27a7bc
sejoroajose <53483859+sejoroajose@users.noreply.github.com>
2024-12-31 16:05:59 -0500

    main

--- client/src/Covenants.jsx
+++ client/src/Covenants.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { ImageIcon, Share2, Download, Lock } from 'lucide-react'
 import {
   Card,
